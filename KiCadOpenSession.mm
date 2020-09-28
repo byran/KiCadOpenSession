f7723566f3a99deb018fdc27c14a39ad6f5156d2
Byran Wills-Heath <byran@adgico.co.uk>
2020-09-28 14:09:10 +0100

    Add Freeplane mind map

---
+++ KiCadOpenSession.mm
@@ -0,0 +1,127 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="KiCad open session" FOLDED="false" ID="ID_1447301221" CREATED="1599555016770" MODIFIED="1599555062806"><hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Project manager" POSITION="right" ID="ID_915283484" CREATED="1599555078299" MODIFIED="1599555081175">
+<node TEXT="Creating a project" ID="ID_1784125444" CREATED="1599555063941" MODIFIED="1599555070614"/>
+<node TEXT="Navigating project and the buttons" ID="ID_358553350" CREATED="1599555101506" MODIFIED="1599555111615"/>
+</node>
+<node TEXT="Schematic editor (eschema)" POSITION="right" ID="ID_1101155736" CREATED="1599555164610" MODIFIED="1599555173685">
+<node TEXT="Moving &amp; zooming" ID="ID_1993378214" CREATED="1599555223322" MODIFIED="1599555227965">
+<node TEXT="Middle button" ID="ID_385788403" CREATED="1599555228722" MODIFIED="1599555232797"/>
+<node TEXT="F4" ID="ID_1320493076" CREATED="1599555233282" MODIFIED="1599555234782"/>
+<node TEXT="Scroll wheel" ID="ID_545261543" CREATED="1600726765152" MODIFIED="1600726795335"/>
+</node>
+<node TEXT="Buttons" ID="ID_5129207" CREATED="1600725663464" MODIFIED="1600725669040">
+<node TEXT="Left for adjusting view" ID="ID_805848308" CREATED="1600725670626" MODIFIED="1600725675329"/>
+<node TEXT="Right for placing things" ID="ID_1494045496" CREATED="1600725676351" MODIFIED="1600725681525"/>
+</node>
+<node TEXT="Placing symbols" ID="ID_976815509" CREATED="1599555293578" MODIFIED="1599555299845"/>
+<node TEXT="Placing wires" ID="ID_1258916746" CREATED="1599555300426" MODIFIED="1599555317173"/>
+<node TEXT="Placing power symbols" ID="ID_501613161" CREATED="1599555324130" MODIFIED="1599555588531"/>
+<node TEXT="Labels" ID="ID_949943955" CREATED="1599555519089" MODIFIED="1599555790813">
+<node TEXT="Local" ID="ID_1523852571" CREATED="1599555521537" MODIFIED="1599555523222"/>
+<node TEXT="Global" ID="ID_591451267" CREATED="1599555523545" MODIFIED="1599555525245"/>
+</node>
+<node TEXT="Annotate symbols" ID="ID_96581426" CREATED="1599555276210" MODIFIED="1599555290862"/>
+<node TEXT="Assigning footprints" ID="ID_174121366" CREATED="1599555780289" MODIFIED="1599555785045">
+<node TEXT="Fixing the footprint" ID="ID_142803405" CREATED="1600990478766" MODIFIED="1600990485436"/>
+</node>
+<node TEXT="DRC check" ID="ID_484425081" CREATED="1599555501714" MODIFIED="1599555505525"/>
+<node TEXT="No connect" ID="ID_559471254" CREATED="1599555506138" MODIFIED="1599555511798"/>
+<node TEXT="Power labels" ID="ID_658095954" CREATED="1600990535240" MODIFIED="1600990539477"/>
+</node>
+<node TEXT="PCB editor" POSITION="right" ID="ID_1744628185" CREATED="1599555659688" MODIFIED="1599555662668">
+<node TEXT="Moving &amp; zooming" ID="ID_1382545354" CREATED="1600726749582" MODIFIED="1600726755608">
+<node TEXT="Middle button" ID="ID_423365436" CREATED="1600726757468" MODIFIED="1600726760274"/>
+<node TEXT="F4" ID="ID_603215939" CREATED="1600726779953" MODIFIED="1600726781506"/>
+<node TEXT="Scroll wheel" ID="ID_450121621" CREATED="1600726782410" MODIFIED="1600726786628"/>
+</node>
+<node TEXT="Buttons" ID="ID_1138046532" CREATED="1600726047593" MODIFIED="1600726049129">
+<node TEXT="Left for adjusting view" ID="ID_315658554" CREATED="1600726050343" MODIFIED="1600726055662"/>
+<node TEXT="Right for placing things" ID="ID_738485805" CREATED="1600726056031" MODIFIED="1600726062501"/>
+</node>
+<node TEXT="Setting design rules" ID="ID_50068286" CREATED="1599555664618" MODIFIED="1599555698821"/>
+<node TEXT="Importing from schematic editor" ID="ID_349073804" CREATED="1599555710601" MODIFIED="1599555736229">
+<node TEXT="Visible netlist" ID="ID_1759319048" CREATED="1599555739737" MODIFIED="1599555745341"/>
+<node TEXT="Turn off F.Fab layer" ID="ID_1906287144" CREATED="1600727498519" MODIFIED="1600727504207"/>
+<node TEXT="Spread out the symbols how you&#xa;think they will be easy to route" ID="ID_425675723" CREATED="1600726163366" MODIFIED="1600727339517">
+<node TEXT="Move footprints with M" ID="ID_182803281" CREATED="1600726451697" MODIFIED="1600726460607"/>
+<node TEXT="Rotate with R" ID="ID_1104769256" CREATED="1600726664947" MODIFIED="1600726668390"/>
+<node TEXT="Clarify footprint" ID="ID_1222599255" CREATED="1600727132478" MODIFIED="1600727176085"/>
+<node TEXT="Accidently picking up part of the footprint" ID="ID_63376146" CREATED="1600727152136" MODIFIED="1600727171844"/>
+</node>
+</node>
+<node TEXT="Create PCB outline on Edge Cuts layer" ID="ID_550220668" CREATED="1600725504481" MODIFIED="1600725514349">
+<node TEXT="Space to reset local local origin" ID="ID_1176193394" CREATED="1600990741972" MODIFIED="1600990758548"/>
+</node>
+<node TEXT="Creating filled zones (planes)" ID="ID_1370741211" CREATED="1600990296428" MODIFIED="1600990324160"/>
+<node TEXT="Placing tracks" ID="ID_1755488371" CREATED="1600727413012" MODIFIED="1600727417130">
+<node TEXT="Vias" ID="ID_1159947565" CREATED="1600989522197" MODIFIED="1600989524401"/>
+<node TEXT="Switching layers with page up and page down" ID="ID_386111758" CREATED="1600725561342" MODIFIED="1600725570301"/>
+</node>
+<node TEXT="Stitching filled zones using vias" ID="ID_1829405817" CREATED="1600990667891" MODIFIED="1600990707070">
+<node TEXT="Including filling gaps" ID="ID_1628793111" CREATED="1600990709840" MODIFIED="1600990716049"/>
+</node>
+</node>
+</node>
+</map>
